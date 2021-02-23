7c8bf2be0d4c6359f49351bafaafbda8aa6e0504
Mayank-Behl <37586705+Mayank-Behl@users.noreply.github.com>
2021-02-23 21:14:25 +0530

    Added Local Storage

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const new_book_button = document.querySelector(".new-book-button");
 const popup = document.querySelector("#popup");
 const span = document.querySelector(".close");
+const addBook = document.querySelector("#addBook");
 
-new_book_button.addEventListener("click", addBookToLibrary);
+addBook.addEventListener("click", addBookToLibrary);
 
 new_book_button.onclick = () => {
   popup.style.display = "block";
@@ -18,18 +19,18 @@ window.onclick = function (event) {
   }
 };
 
-let myLibrary = [];
-let newBook;
-
 //BOOK CLASS
 class Book {
   constructor(title, author, pages, read) {
     this.title = form.title.value;
     this.author = form.author.value;
-    this.title = form.pages.value + "page(s)";
+    this.pages = form.pages.value + " page(s)";
     this.read = form.read.checked;
   }
 }
+
+let myLibrary = [];
+let newBook;
 
 //Creating a new book using Book class and adding the created book to library
 
@@ -38,8 +39,20 @@ function addBookToLibrary(event) {
   popup.style.display = "none";
   newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  setData(); 
   render();
   form.reset();
+}
+
+/* [-----------------CREATING THE DATA IN BROWSER-----------------] */
+function render() {
+  const display = document.querySelector("#library-container");
+  const books = document.querySelectorAll(".book");
+  books.forEach((book) => display.removeChild(book));
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBook(myLibrary[i]);
+  }
 }
 
 /* [-----------------CREATING DOM ELEMENTS for library container-----------------] */
@@ -70,38 +83,50 @@ function createBook(item) {
   readBtn.classList.add("readBtn");
   bookDiv.appendChild(readBtn);
   if (item.read === false) {
-    readBtn.textContent = "Not Read &#10005;";
+    readBtn.textContent = "Not Read";
     readBtn.style.background = "#e04f63";
   } else {
-    readBtn.textContent = "Read &#10003;";
+    readBtn.textContent = "Read";
     readBtn.style.background = "#63da63";
   }
 
   removeBtn.textContent = "Remove";
-  removeBtn.setAttribute("id", removeBtn);
+  removeBtn.setAttribute("id", "removeBtn");
   bookDiv.appendChild(removeBtn);
 
   library.appendChild(bookDiv);
 
   removeBtn.addEventListener("click", () => {
     myLibrary.splice(myLibrary.indexOf(item), 1);
+    setData(); 
     render();
   });
 
   //add toggle ability to each book 'read' button on click
   readBtn.addEventListener("click", () => {
     item.read = !item.read;
+    setData(); 
     render();
   });
 }
 
-/* [-----------------CREATING THE DATA IN BROWSER-----------------] */
-function render() {
-  const display = document.querySelector("#library-container");
-  const books = document.querySelectorAll(".book");
-  books.forEach((book) => display.removeChild(book));
+//LOCAL STORAGE
 
-  for (let i = 0; i < myLibrary.length; i++) {
-    createBook(myLibrary[i]);
+// Setting up for local storage in JSON format 
+function setData(){
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+// Getting data from the local stortage
+function restore(){
+  if(!localStorage.myLibrary){
+    render();
+  }else{
+    let objects = localStorage.getItem('myLibrary');
+    objects = JSON.parse(objects);
+    myLibrary = objects;
+    render();
   }
 }
+
+restore();
